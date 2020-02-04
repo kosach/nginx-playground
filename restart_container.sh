cba7f6413e4fa10183b048565f62f0a01cc1569b
Andrii Kosach <kosach.a@gmail.com>
2020-02-04 10:46:04 +0200

    Added mathes

--- restart_container.sh
+++ restart_container.sh
@@ -1,5 +1,16 @@
+
+
+#!/bin/bash
+
+NETWORK_NAME="test-microservices-network"
+if [ $(docker network ls|grep -c $NETWORK_NAME) = 0 ]
+then 
+   docker network create $NETWORK_NAME;
+   echo "Created network $NETWORK_NAME"
+fi
+
 docker stop nginx_playgraund
 docker build --tag nginx.playgraund .
 docker rm nginx_playgraund
-docker run -p 8080:80 --name nginx_playgraund --net=test-microservices-network nginx.playgraund
+docker run -p 8080:80 --name nginx_playgraund --net=$NETWORK_NAME nginx.playgraund
 
